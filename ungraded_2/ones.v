32ec4817b3f49d5e1f93b1766ef21f2e72274ca6
minecraftdixit <63745645+minecraftdixit@users.noreply.github.com>
2023-10-21 17:10:42 +0530

    Update ones.v

--- ungraded_2/ones.v
+++ ungraded_2/ones.v
@@ -1,30 +1,15 @@
-module count_ones #(parameter WIDTH = 10)
-(
-    input wire clk,
-    input wire reset,
-    input wire data_in,
-    output reg [3:0] ones_count
-);
+module one(
+    input [9:0] A,
+    output reg [4:0] ones
+    );
 
-reg [WIDTH-1:0] shift_reg;
 integer i;
-integer count;
 
-always @(posedge clk or posedge reset) begin
-    if (reset) begin
-        shift_reg <= 0;
-        ones_count <= 0;
-    end else begin
-        shift_reg <= {shift_reg[WIDTH-2:0], data_in};
-        
-        count = 0;
-        for (i = 0; i < WIDTH; i = i + 1) begin
-            if (shift_reg[i])
-                count = count + 1;
-        end
-        
-        ones_count <= count;
-    end
+always@(A)
+begin
+    ones = 0;  //initialize count variable.
+    for(i=0;i<10;i=i+1)    
+        ones = ones + A[i];  
 end
 
 endmodule
