087800ac41084fae2e05c04e9c4632d4178214ca
minecraftdixit <63745645+minecraftdixit@users.noreply.github.com>
2023-10-21 17:12:38 +0530

    Update top.v

--- ungraded_2/top.v
+++ ungraded_2/top.v
@@ -1,32 +1,25 @@
-`timescale 1ns / 1ps
-
-module top_module(
-    input wire clk,             // Clock input
-    input wire rst,             // Reset input
-    input wire data_in,         // Input data stream
-    output wire [3:0] count_out, // Output count every 10 bits
-    output wire detect_out      // Output signal when sequence detected
+module top_module (
+  input bit clk,
+  input rst_n,
+  input [9:0] data_in,
+  input x , 
+  output reg [4:0] ones_out,
+  output z
 );
 
-wire sequence_detected; // Wire to carry the detection flag
-
-// Instantiate the ones-Counter module
-ones_counter ones_counter_inst (
+ 
+  fsm sequence_detector (
     .clk(clk),
-    .rst(rst),
-    .data_in(data_in),
-    .count_out(count_out)
-);
-
-// Instantiate the sequence detector FSM
-sequence_detector sequence_detector_inst (
-    .clk(clk),
-    .rst(rst),
-    .data_in(data_in),
-    .detect_out(sequence_detected)
-);
-
-// Output the detection flag from the sequence detector
-assign detect_out = sequence_detected;
+    .rst_n(rst_n),
+    .x(x),  
+    .z(z)
+  );
+ 
+  one counter (
+    .A(data_in),
+    .ones(ones_out)
+  );
+ 
+ 
 
 endmodule
